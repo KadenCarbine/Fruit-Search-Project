3e5ceaef89de19e410b9e49b01b1c779e6a2f7e5
KadenCarbine <Kadenjcar@gmail.com>
2023-06-09 14:38:45 -0600

    Search Function Completed and Working

---
+++ script.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+//Loop through the array and do a search on each word to see if the current sting is in that word and if it is add it to the results array
+
+function search(str) {
+	let results = [];
+	for(let fruit of fruits) {
+		let string = str.toLowerCase();
+		if(fruit.toLowerCase().includes(string)) {
+			results.push(fruit)
+		}
+	}
+	return results;
+}
+
+function searchHandler(e) {
+	// TODO
+}
+
+function showSuggestions(results, inputVal) {
+
+	// TODO
+}
+
+function useSuggestion(e) {
+	// TODO
+}
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
