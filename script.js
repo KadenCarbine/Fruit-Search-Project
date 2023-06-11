8b2ca7ee6ed82901c34a63ef344b350b2cac1749
KadenCarbine <Kadenjcar@gmail.com>
2023-06-11 10:38:03 -0600

    Trying again, searchHandler function is complete with comments

--- script.js
+++ script.js
@@ -3,34 +3,45 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-//Loop through the array and do a search on each word to see if the current sting is in that word and if it is add it to the results array
+
 
 function search(str) {
+	//Initialize the results array that the current value of the input has
 	let results = [];
+	//Loop through the fruits array that was alredy filled with different fruits
 	for(let fruit of fruits) {
+		//Make the input that someone types into all lowercase to be able to compare to the fruits array
 		let string = str.toLowerCase();
+		//make the fruits array lowercase to compare and see if it includes the current string at any point
 		if(fruit.toLowerCase().includes(string)) {
+			//push the fruit that have the string included to the results array
 			results.push(fruit)
 		}
 	}
 	return results;
 }
-//When a key is pressed Loop through the results array and make each one into an li and put it in the suggestions ul and make that appear on the screen in a dropdown menu
 
-//figure out how to make it so that when something is typed into the type=text that it makes it go through the seach function so that it can create a results array that I would be able to loop through and do my first thought currently seachHandler(e) e.target console.logs the whole html line?
 
 
 function searchHandler(e) {
+	//Make the inputVal the current value of the textbox when a new keyup happens
 	let inputVal = e.target.value;
-	
+	//Initialize results as an empty array
 	let results = [];
-	results = search(inputVal)
-	console.log(results)
+	//If inputVal.length is = 0 then nothing will happen, but if it is a number it is truthy and the if statement goes through
+	if(inputVal.length) {
+		//have the inputVal be the argument for the search function made earlier, and set it to the results variable
+		results = search(inputVal)
+	}
+	//?Idea? This will call the next function that I will make so that it will show the suggestions on the screen
+	showSuggestions(results, inputVal)
 }
+
+
 
 function showSuggestions(results, inputVal) {
 
-	// TODO
+
 }
 
 function useSuggestion(e) {
